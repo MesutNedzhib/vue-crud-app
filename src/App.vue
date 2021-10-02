830e2c9f73a513bb9d7207d266a873af511246d6
swaperX <mesut.nedzhib@gmail.com>
2021-10-02 16:09:10 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,60 @@
 <template>
   <div id="app">
-    <ProductAdd @add:product="addProduct"/>
-    <ProductList @delete:product="deleteProduct" @update:product="updateProduct" :products="products" />
+    <ProductAdd @add:product="addProduct" />
+    <ProductList
+      @delete:product="deleteProduct"
+      @update:product="updateProduct"
+      :products="products"
+    />
   </div>
 </template>
 
-
-
 <script>
 import ProductList from "./components/ProductList.vue";
-import ProductAdd from './components/ProductAdd.vue';
+import ProductAdd from "./components/ProductAdd.vue";
 
 export default {
   name: "App",
   components: {
     ProductList,
-    ProductAdd
+    ProductAdd,
   },
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          categoryId: 1,
-          productName: "Phone",
-          quantityPerUnit: "Iphone X",
-          unitPrice: 1199.99,
-          unitsInStock: 2,
-        },
-        {
-          id: 2,
-          categoryId: 2,
-          productName: "Phone",
-          quantityPerUnit: "Xiaomi Note 10",
-          unitPrice: 299.99,
-          unitsInStock: 5,
-        },
-        {
-          id: 3,
-          categoryId: 3,
-          productName: "Phone",
-          quantityPerUnit: "Iphone XIII",
-          unitPrice: 1799.99,
-          unitsInStock: 3,
-        },
-      ],
+      products: [],
     };
   },
-  methods:{
-    deleteProduct(product){
-      this.products = this.products.filter(x=>x.id !== product.id)
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    async getProducts() {
+      const result = await fetch("http://localhost:3000/products");
+      const data = await result.json();
+      this.products = data;
     },
-    updateProduct(){
+    async deleteProduct(product) {
+      await fetch("http://localhost:3000/products/"+product.id,{
+        method: "DELETE"
+      })
+
+      this.products = this.products.filter((x) => x.id !== product.id);
+    },
+    async updateProduct() {
       // api
     },
-    addProduct(product){
-      const newProduct = {...product} // create new ref in memory
-      this.products = [...this.products,newProduct]
-    }
-  }
+    async addProduct(product) {
+      const result = await fetch("http://localhost:3000/products", {
+        method: "POST",
+        body: JSON.stringify(product),
+        headers: { "Content-Type": "application/json" },
+      });
+      const newProduct = await result.json(); // create new ref in memory
+      this.products = [...this.products, newProduct];
+    },
+  },
 };
 </script>
-
-
 
 <style>
 #app {
